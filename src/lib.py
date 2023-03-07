2b043335340d923a486ec66eba564cf5464d7896
Ash-Manoj <ashish.jaseetha@kit.edu>
2023-03-07 16:05:30 +0100

    Adding lib.py file

---
+++ src/lib.py
@@ -0,0 +1,13 @@
+from wbt import wbt
+
+def print_info(to_file: bool):
+    info = wbt.version()
+
+    # write to file if needed
+    if to_file:
+        with open('/out/INFO.txt', 'w') as f:
+            f.write(info)
+
+    # output 
+    print('I am updated')
+    print(info)
