bbb1203655d99d25755141cb6ece49f4b218548d
Ash-Manoj <ashish.jaseetha@kit.edu>
2023-03-07 16:05:21 +0100

    Adding lib.py to call functions

--- src/run.py
+++ src/run.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 from pprint import pprint
 
 from json2args import get_parameter
-from wbt import wbt
+import lib as wblib
 
 # parse parameters
 kwargs = get_parameter()
@@ -13,15 +13,7 @@ toolname = os.environ.get('TOOL_RUN', 'whitebox_info').lower()
 
 # switch the tool
 if toolname == 'whitebox_info':
-    info = wbt.version()
-
-    # write to file if needed
-    if kwargs.get('toFile', True):
-        with open('/out/INFO.txt', 'w') as f:
-            f.write(info)
-
-    # output 
-    print(info)
+    wblib.print_info(to_file=kwargs.get('toFile', True))
 
 # In any other case, it was not clear which tool to run
 else:
